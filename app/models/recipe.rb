4ed439b339436c4832008a7c2c8a6ff3d9cf06ce
Neniah <maria.lobillo.santos@gmail.com>
2015-06-16 18:18:21 +0200

    add coccon, add model: ingredient, direction

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ActiveRecord::Base
+
+  has_many :ingredients
+  has_many :directions
+  
   has_attached_file :image, styles: { :medium => "400x400>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
